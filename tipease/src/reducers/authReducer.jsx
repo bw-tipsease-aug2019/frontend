e698129a0d6f2797fcebd6452441601f87338902
skyler dowdy <skylerwebdev@gmail.com>
2019-08-26 10:22:29 -0400

    create user and login reducer

--- tipease/src/reducers/authReducer.jsx
+++ tipease/src/reducers/authReducer.jsx
@@ -1,13 +1,67 @@
 import { types } from "../actions";
 
 const initialState = {
- 
+  token: '',
+  user: null,
+  isAuth: false,
+  isLoading: false,
+  isSuccess: false,
+  errors: null
 };
 
 export default (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
-
+    case types.CREATE_USER_START:
+      return{
+        ...state,
+        isLoading: true,
+        isAuth: false,
+        isSuccess: false,
+        errors: null,
+        user: {}
+      }
+    case types.CREATE_USER_SUCCESS:
+      return{
+        ...state,
+        isLoading: false,
+        isAuth: false,
+        isSuccess: true,
+        errors: null,
+        user: {}
+      }
+    case types.CREATE_USER_FAIL:
+      return{
+        ...state,
+        isLoading: false,
+        isAuth: false,
+        isSuccess: false,
+        errors: payload,
+        user: {}
+      }
+    case types.LOGIN_START:
+      return{
+        ...state,
+        isLoading: true,
+        isAuth: false,
+        isSuccess: false,
+        errors: null
+      }
+    case types.LOGIN_SUCCESS:
+      return{
+        ...state,
+        isLoading: false,
+        isAuth: true,
+        isSuccess: true,
+        user: payload.user,
+        token: payload.token
+      }
+    case types.LOGIN_FAIL:
+      return{
+        ...state,
+        errors: payload,
+        isLoading: false
+      }
     default:
       return state;
   }
