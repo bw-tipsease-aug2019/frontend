1874450e28cacf743e95af116e685440b66dac02
danRolon <daniel.rolon@yahoo.com>
2019-08-26 13:22:10 -0500

    setting up list of workers and search form to filter the list

--- tipease/src/components/serviceWorker/workerList.jsx
+++ tipease/src/components/serviceWorker/workerList.jsx
@@ -4,3 +4,49 @@
 /          -List displayed in grid format
 /          -Allow list to sort by worker role
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+import React, {useEffect} from 'react';
+import SearchForm from './workerSearchForm';
+import { useDispatch, useSelector } from 'react-redux';
+import {getWorkers} from '../actions/index';
+
+const WorkerList = props => {
+  const workers = useSelector(state => state.workers);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  let filteredWorkers = [...workers];
+  if(filter.company){
+    filteredWorkers = filteredWorkers.filter(worker=>worker.company===filter.company);
+  }
+
+  if(filter.role){
+    filteredWorkers = filteredWorkers.filter(worker=>worker.role===filter.role);
+  }
+
+  if(filter.name){
+    filteredWorkers = filteredWorkers.filter(worker=>worker.name.includes(filter.name));
+  }
+
+  useEffect(()=>{
+    dispatch(getWorkers());
+  },[]);
+
+  return (
+      <div className='searchParameters'>
+        <SearchForm />
+      </div>
+
+      <div className='workersGrid'>
+        {filteredWorkers.map(worker => {
+          return (
+            <div className='workersGrid-item'>
+              <WorkerCard key={worker.id} worker={worker} />
+            </div>
+          );
+        })}
+      </div>
+  );
+}
+
+export default WorkerList;
