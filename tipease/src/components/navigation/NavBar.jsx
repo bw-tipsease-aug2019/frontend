a074551f27ab2a758afc613fb925625fdcc353e0
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-27 15:58:07 -0700

    remove "Hello Tipsease" and style nav
    Co-authored-by: Chris Bonifacio <christopher.bonifacio@gmail.com>
    Co-authored-by: Ramon Audain <raudain123@gmail.com>

--- tipease/src/components/navigation/NavBar.jsx
+++ tipease/src/components/navigation/NavBar.jsx
@@ -14,8 +14,8 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
+      <nav className="nav-bar">
+        <Link to="/">tipsease</Link>
       </nav>
     </div>
   );
