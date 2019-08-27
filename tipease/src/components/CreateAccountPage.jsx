5aa9544812231f902d0e9b5d98e2709d958ededc
skyler dowdy <skylerwebdev@gmail.com>
2019-08-27 09:50:31 -0400

    send basic props to create account so that it has history push method

--- tipease/src/components/CreateAccountPage.jsx
+++ tipease/src/components/CreateAccountPage.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import CreateAccountForm from './forms/CreateAccount'
 import { Link } from 'react-router-dom'
 
-export default function CreateAccountPage() {
+export default function CreateAccountPage(props) {
     return (
         <div className="create-account-page">
-            <CreateAccountForm />
+            <CreateAccountForm props={props}/>
             <p>Already a member? <Link to='/protected'>Sign in</Link></p>
         </div>
     )
