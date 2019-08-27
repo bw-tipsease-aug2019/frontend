aaf65f240fbc8a0833ee0c50fd8df198c999cf11
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-08-27 13:46:35 -0400

    added uniformity to worker cards

--- tipease/src/components/serviceWorker/WorkerCard.jsx
+++ tipease/src/components/serviceWorker/WorkerCard.jsx
@@ -13,26 +13,53 @@
 /
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { useDispatch } from "react-redux";
 //import {removeWorker} from '../actions/workerActions';
 
 const WorkerCard = props => {
   const dispatch = useDispatch();
   return (
-    <div className='worker-card'>
-      <h2>{props.worker.name.first} {props.worker.name.last}</h2>
-      <img className='worker-thumbnail' alt='Worker Photo Here' src={props.worker.thumbnail} />
-      <div>
-        <p>{props.worker.role}</p>
-        <p>{props.worker.tagline}</p>
-        <p>Employed for</p>
-        <p>{props.worker.durationEmployed.year} years and {props.worker.durationEmployed.month} months.</p>
-        <div className='ui button' onClick={()=>{props.redirect(`/tip/${props.worker.id}`)}}>Tip</div>
-        <div className='ui button' onClick={()=>{props.redirect(`/worker/${props.worker.id}`)}}>More Info</div>
+    <div className="worker-card">
+      <div className="worker-info">
+        <h2>
+          {props.worker.name.first} {props.worker.name.last}
+        </h2>
+        <img
+          className="worker-thumbnail"
+          alt="Worker Photo Here"
+          src={props.worker.thumbnail}
+        />
+        <div>
+          <p>{props.worker.role}</p>
+          <p>{props.worker.tagline}</p>
+          <p>Employed for</p>
+          <p>
+            {props.worker.durationEmployed.year} years and{" "}
+            {props.worker.durationEmployed.month} months.
+          </p>
+        </div>
+      </div>
+      <div className="worker-card-buttons">
+        <div
+          className="ui button"
+          onClick={() => {
+            props.redirect(`/tip/${props.worker.id}`);
+          }}
+        >
+          Tip
+        </div>
+        <div
+          className="ui button"
+          onClick={() => {
+            props.redirect(`/worker/${props.worker.id}`);
+          }}
+        >
+          More Info
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default WorkerCard;
