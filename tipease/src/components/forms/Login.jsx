e6868507b58575a29ea2886913af47b94a8e4e18
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-28 14:10:18 -0700

    add CoffeeShopBg to Login form

--- tipease/src/components/forms/Login.jsx
+++ tipease/src/components/forms/Login.jsx
@@ -2,30 +2,34 @@ import React from "react";
 import { connect } from "react-redux";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+
 import { doSignIn } from "../../store/actions/authActions";
+import CoffeeShopBg from '../CoffeeShopBg'
 
 function LogFrm({ errors, touched }) {
   return (
-    <div className="form-card">
-      <h1>Login</h1>
-      <Form className="ui form">
-        <div className="field">
-          {touched.email && errors.email && (
-            <p className="error">{errors.email}</p>
-          )}
-          <Field type="text" name="email" placeholder="Email" />
-        </div>
-        <div className="field">
-          {touched.password && errors.password && (
-            <p className="error">{errors.password}</p>
-          )}
-          <Field type="password" name="password" placeholder="Password" />
-        </div>
-        <button className="ui button" type="submit" onClick={() => {}}>
-          Login
-        </button>
-      </Form>
-    </div>
+    <CoffeeShopBg>
+      <div className="form-card">
+        <h1>Login</h1>
+        <Form className="ui form">
+          <div className="field">
+            {touched.email && errors.email && (
+              <p className="error">{errors.email}</p>
+            )}
+            <Field type="text" name="email" placeholder="Email" />
+          </div>
+          <div className="field">
+            {touched.password && errors.password && (
+              <p className="error">{errors.password}</p>
+            )}
+            <Field type="password" name="password" placeholder="Password" />
+          </div>
+          <button className="ui button" type="submit" onClick={() => {}}>
+            Login
+          </button>
+        </Form>
+      </div>
+    </CoffeeShopBg>
   );
 }
 
