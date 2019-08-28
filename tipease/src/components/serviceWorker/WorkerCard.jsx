62dae752a950d63bcd06c0284e66f7d5bf1520e5
skyler dowdy <skylerwebdev@gmail.com>
2019-08-28 09:23:27 -0400

    re add login ternary

--- tipease/src/components/serviceWorker/WorkerCard.jsx
+++ tipease/src/components/serviceWorker/WorkerCard.jsx
@@ -43,14 +43,25 @@ const WorkerCard = props => {
       </div>
 
       <div className="worker-card-buttons">
-        <div
-          className="ui button"
-          onClick={() => {
-            props.redirect(`/tip/${props.worker.id}`);
-          }}
-        >
-          Tip
-        </div>
+      {!localStorage.getItem("token") ? (
+          <div
+            className="ui button"
+            onClick={() => {
+              props.redirect(`/login`);
+            }}
+          >
+            Login
+          </div>
+        ) : (
+          <div
+            className="ui button"
+            onClick={() => {
+              props.redirect(`/tip/${props.worker.id}`);
+            }}
+          >
+            Tip
+          </div>
+        )}
 
         <div
           className="ui button"
@@ -62,29 +73,7 @@ const WorkerCard = props => {
         </div>
       </div>
 
-      {/* {!localStorage.getItem("token") ? (
-        
-      ) : (
-        <div className="worker-card-buttons">
-          <div
-            className="ui button"
-            onClick={() => {
-              props.redirect(`/tip/${props.worker.id}`);
-            }}
-          >
-
-          </div>
-
-          <div
-            className="ui button"
-            onClick={() => {
-              props.redirect(`/worker/${props.worker.id}`);
-            }}
-          >
-            More Info
-          </div>
-        </div>
-      )} */}
+      
     </div>
   );
 };
