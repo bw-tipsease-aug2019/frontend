a7d9c9df36153f9c9b5bb92200c1e54579097f7f
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-28 09:57:41 -0700

    add coffee shop background component + wrap it around other components
    Co-authored-by: Chris Bonifacio <christopher.bonifacio@gmail.com>

---
+++ tipease/src/components/CoffeeShopBg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CoffeeShopBg(props) {
+    return (
+        <div className="coffee-shop-bg">
+            {props.children}
+        </div>
+    )
+}
--- tipease/src/components/serviceWorker/tipForm.jsx
+++ tipease/src/components/serviceWorker/tipForm.jsx
@@ -5,6 +5,7 @@ import * as Yup from "yup";
 import { useSelector, connect } from 'react-redux';
 
 import {getWorker} from '../../store/actions/workerActions'
+import CoffeeShopBg from '../CoffeeShopBg'
 
 function TipFrm(props) {
   const worker = useSelector(state=>state.workerReducer.currentWorker);
@@ -12,32 +13,35 @@ function TipFrm(props) {
   useEffect(()=>{
     props.getWorker(props.match.params.id);
   },[]);
-  return (
-    <div className="form-card">
-      {worker.name?
-        <h1>Tipping {`${worker.name.first} ${worker.name.last}`}</h1> :
-        <></>  
-      }
-      
-      <Form className="ui form">
-        <div className="field">
-          {props.touched.amount && props.errors.amount && <p>{props.errors.amount}</p>}
-          <label htmlFor='amount'>Amount: </label>
-          <Field type="number" name="amount" id='amount' placeholder="$5.00" />
-        </div>
-        <div className="field">
-        <label htmlFor='comment'>Leave a comment: </label>
-          <Field type="text" name="comment" id="comment" placeholder="Add a comment" />
-        </div>
 
-        <div className="form-buttons">
-          <button className="ui button" type="submit">
-            Tip
-          </button>
-          <button className='ui button' onClick={()=>{props.history.push('/')}}>Back</button>
-        </div>
-      </Form>
-    </div>
+  return (
+    <CoffeeShopBg>
+      <div className="form-card">
+        {worker.name?
+          <h1>Tipping {`${worker.name.first} ${worker.name.last}`}</h1> :
+          <></>  
+        }
+        
+        <Form className="ui form">
+          <div className="field">
+            {props.touched.amount && props.errors.amount && <p>{props.errors.amount}</p>}
+            <label htmlFor='amount'>Amount: </label>
+            <Field type="number" name="amount" id='amount' placeholder="$5.00" />
+          </div>
+          <div className="field">
+          <label htmlFor='comment'>Leave a comment: </label>
+            <Field type="text" name="comment" id="comment" placeholder="Add a comment" />
+          </div>
+
+          <div className="form-buttons">
+            <button className="ui button" type="submit">
+              Tip
+            </button>
+            <button className='ui button' onClick={()=>{props.history.push('/')}}>Back</button>
+          </div>
+        </Form>
+      </div>
+    </CoffeeShopBg>
   );
 }
 
