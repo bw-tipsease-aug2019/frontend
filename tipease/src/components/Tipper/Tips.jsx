b2048627f6491b7c3a500ab62c889dcafb8fcad5
skyler dowdy <skylerwebdev@gmail.com>
2019-08-29 10:20:23 -0400

    tips display to page

--- tipease/src/components/Tipper/Tips.jsx
+++ tipease/src/components/Tipper/Tips.jsx
@@ -1,25 +1,41 @@
-import React, {useEffect} from "react";
-import {connect} from 'react-redux'
-import  {getTipList} from '../../store/actions/tipActions'
-import TipCard from "./TipCard";
-
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getTipList } from "../../store/actions/tipActions";
 const Tips = props => {
-console.log("TCL: props", props)
-    useEffect(() => {
-        props.getTipList()
-    },[])
+  console.log("TCL: props", props);
+  useEffect(() => {
+    props.getTipList();
+  }, []);
+  
+  const add = (a,b) => a + b 
 
-    return(
-        <>
-        { props.tips && props.tips.map(tip => (
-            <TipCard key={tip.id} tip={tip} />
-    ))}
-        </>
-    )
-}
+  return (
+    <>
+      {props.tips &&
+        props.tips.map(tip => (
+          <>
+            <div className="tip-card-wrapper">
+              <div className="tip-card">
+                <div className="tip-card-amount">
+                  <p>{`Tip Amount: $${tip.tipAmount}`}</p>
+                </div>
+                <div className="tip-card-title">
+                  <p>{`Comment: ${tip.comment}`}</p>
+                </div>
+              </div>
+            </div>
+  
+          </>
+        ))}
+    </>
+  );
+};
 
-const mapStateToProps = (state) => ({
-    tips: state.tipReducer.tipList,
-})
+const mapStateToProps = state => ({
+  tips: state.tipReducer.tipList
+});
 
-export default connect(mapStateToProps, {getTipList})(Tips)
+export default connect(
+  mapStateToProps,
+  { getTipList }
+)(Tips);
