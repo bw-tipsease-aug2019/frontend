7535aa8869a2d978bc91bae26e1da7eb84461d04
skyler dowdy <skylerwebdev@gmail.com>
2019-08-29 11:51:52 -0400

    profile update

--- tipease/src/components/Profile.jsx
+++ tipease/src/components/Profile.jsx
@@ -1,28 +1,25 @@
-import React, {useEffect} from 'react';
-import {connect} from 'react-redux'
-import {doGetProfile} from '../store/actions/authActions'
-import '../App.scss';
-import CoffeeShopBg from './CoffeeShopBg';
-import ProfileCard from './ProfileCard';
-
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { doGetProfile } from "../store/actions/authActions";
 const Profile = props => {
-    console.log("TCL: props", props)
-    const isServiceWoker = localStorage.getItem('serviceWorker')
-    console.log("TCL: isServiceWoker", isServiceWoker)
-    useEffect(() => {
-        props.doGetProfile()
-    }, [])
+  console.log("TCL: props", props);
+  useEffect(() => {
+    props.doGetProfile();
+  }, []);
+  
 
-    return(
-        <>
-        {props.userProfile &&
-        <ProfileCard props={props.userProfile} />
-        }
-        </>
-    )
-}
+  return (
+    <>
+      <h1>{props.userProfile && props.userProfile.firstName + " " + props.userProfile.lastName}</h1>
+    </>
+  );
+};
 
-const mapStateToProps = (state) => ({
-    userProfile: state.authReducer.userProfile
-})
-export default connect(mapStateToProps, {doGetProfile})(Profile)
+const mapStateToProps = state => ({
+  userProfile: state.authReducer.userProfile
+});
+
+export default connect(
+  mapStateToProps,
+  { doGetProfile }
+)(Profile);
